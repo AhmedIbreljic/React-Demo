5f474148827cf2ca716705714d4a7fb7a7e91851
Ahmed Ibreljic <ahmedibreljic@gmail.com>
2024-04-15 20:53:49 -0400

    Update TopBanner.tsx

--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -5,15 +5,14 @@ export default function TopBanner() {
     <div className='top-0 sticky z-[110] isolate flex items-center gap-x-6 overflow-hidden bg-gradient-to-r from-brand-blue to-brand-light-blue px-6 py-2.5 sm:px-3.5 sm:before:flex-1 bg-'>
       <div className='flex flex-wrap items-center gap-x-4 gap-y-2'>
         <p className='text-sm font-medium leading-6 text-white'>
-          ✨ Welcome to the Frigade demo app. Everything you see here was built
-          using the Frigade React SDK
+          ✨ Welcome to the EduX demo app. Everything you see here was built by university students, for students. 
         </p>
         <a
-          href='https://cal.com/team/frigade/frigade-demo?duration=20'
+          href='https://cal.com/edux-team/15min'
           target='_blank'
           className='flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900'
         >
-          Schedule demo <span aria-hidden='true'>&rarr;</span>
+          Schedule demo walkthrough with EduX <span aria-hidden='true'>&rarr;</span>
         </a>
       </div>
       <div className='flex flex-1 justify-end'></div>
